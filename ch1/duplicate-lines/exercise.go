1ce54f987e91c892277872f4ad1b338b264251be
frederickalcantara <frederickaalcantara@gmail.com>
2019-03-31 09:43:18 -0400

    Added exercise for duplicate lines

---
+++ ch1/duplicate-lines/exercise.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+	for file, textFiles := range files {
+		for line, n := range textFiles {
+			if n > 1 {
+				fmt.Printf("%d %s\n", file, textFiles, line)
+			}
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
