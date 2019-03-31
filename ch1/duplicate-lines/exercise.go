0ee4df53a654be76f8064c6f62fc609e1c4606cb
frederickalcantara <frederickaalcantara@gmail.com>
2019-03-31 10:04:35 -0400

    solved exercise for duplicate lines

--- ch1/duplicate-lines/exercise.go
+++ ch1/duplicate-lines/exercise.go
@@ -8,9 +8,10 @@ import (
 
 func main() {
 	counts := make(map[string]int)
+	fileCount := make(map[string]map[string]int)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts, fileCount)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -18,22 +19,31 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, counts, fileCount)
 			f.Close()
 		}
 	}
-	for file, textFiles := range files {
-		for line, n := range textFiles {
-			if n > 1 {
-				fmt.Printf("%d %s\n", file, textFiles, line)
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s", n, line)
+			for fn, fc := range fileCount[line] {
+				fmt.Printf("\n\t -> %dx in %s", fc, fn)
 			}
+			fmt.Println()
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]int, fileCount map[string]map[string]int) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
-		counts[input.Text()]++
+		text := input.Text()
+		fileName := f.Name()
+		counts[text]++
+		if fileCount[text] == nil {
+			fileCount[text] = make(map[string]int)
+		}
+		fileCount[text][fileName]++
 	}
+	// NOTE: ignoring potential errors from input.Err()
 }
