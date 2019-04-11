9e34906a471a601b356c02c413e79dd8a3d9a873
frederickalcantara <frederickaalcantara@gmail.com>
2019-04-10 22:25:48 -0400

    Added popcount of number of bits

---
+++ ch2/package-init/popcount.go
@@ -0,0 +1,33 @@
+package popcount
+
+import "fmt"
+
+// pc[i] is the population count of i.
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// PopCount returns the population count (number of set bits) of x.
+func PopCountTable(x uint64) int {
+	sum := 0
+	for i := uint(0); i < 8; i++ {
+		sum += int(pc[byte(x>>i*8)])
+	}
+	fmt.Println(sum)
+	return sum
+}
+
+func PopCountList(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
