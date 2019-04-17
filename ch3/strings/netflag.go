56f6b6f779e67b5e73da5c0aa2a07ba8dbc42e73
frederickalcantara <frederickaalcantara@gmail.com>
2019-04-17 07:10:46 -0400

    Added string examples

---
+++ ch3/strings/netflag.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+type Flags uint
+
+const (
+	FlagUp           Flags = 1 << iota // is up
+	FlagBroadcast                      // supports broadcast access capability
+	FlagLoopback                       // is a loopback interface
+	FlagPointToPoint                   // belongs to a point-to-point link
+	FlagMulticast                      // supports multicast access capability
+)
+
+func main() {
+	var v Flags = FlagMulticast | FlagUp
+	fmt.Printf("%b %t\n", v, IsUp(v)) // "10001 true"
+	TurnDown(&v)
+	fmt.Printf("%b %t\n", v, IsUp(v)) // "10000 false"
+	SetBroadcast(&v)
+	fmt.Printf("%b %t\n", v, IsUp(v))   // "10010 false"
+	fmt.Printf("%b %t\n", v, IsCast(v)) // "10010 true"
+}
