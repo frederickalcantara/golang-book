bfd636174d149c1421c8d2bd7e44eb697acf0aac
frederickalcantara <frederickaalcantara@gmail.com>
2019-04-19 01:05:59 -0400

    Edited comments

--- ch4/hashing-exc/sha256.go
+++ ch4/hashing-exc/sha256.go
@@ -1,7 +1,5 @@
 // 1. Write a function that counts the number of bits that are different in two SHA256 hashes. (See PopCount code below)
 
-// 2. Write a program that prints the SHA256 hash of its standard input by default but supports a command-line flag to print the SHA384 or SHA512 hash instead.
-
 package main
 
 import (
