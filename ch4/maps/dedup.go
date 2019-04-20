0a9b661d1acb6eb0024539344c1b5f8ad984aad4
frederickalcantara <frederickaalcantara@gmail.com>
2019-04-20 12:56:12 -0400

    added dedup file and moved files around

---
+++ ch4/maps/dedup.go
@@ -0,0 +1,25 @@
+package main
+
+// Uses a map to determine which represent which set of lines have already appeared to ensure repeating occurencies don't get printed
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	seen := make(map[string]bool) // a set of strings
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		line := input.Text()
+		if !seen[line] {
+			seen[line] = true
+			fmt.Println(line)
+		}
+	}
+	if err := input.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "dedup: %v\n", err)
+		os.Exit(1)
+	}
+}
---
+++ ch4/maps/map.go
@@ -0,0 +1,15 @@
+package maps
+
+// Function used to compare two maps to determine if they have the same keys and values
+
+func maps(x, y map[string]int) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for k, xv := range x {
+		if yv, ok := y[k]; !ok || yv != xv {
+			return false
+		}
+	}
+	return true
+}
