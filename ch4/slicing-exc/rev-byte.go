39cb719b0d8a16346581fab82b022815d648b1ba
frederickalcantara <frederickaalcantara@gmail.com>
2019-04-20 12:45:20 -0400

    removed files

--- ch4/slicing-exc/rev-byte.go
+++
@@ -1,9 +0,0 @@
-// Modify reverse to reverse the characters of a []byte slice that represents a UTF-8-encoded string, in place. Can you do it without allocating new memory?
-
-package main
-
-import "fmt"
-
-func main() {
-
-}
