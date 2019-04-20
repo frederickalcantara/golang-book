0224d3fb410c0cf3bc8d3ef551999ffdc5d53f98
frederickalcantara <frederickaalcantara@gmail.com>
2019-04-20 12:17:38 -0400

    Completed array pointer reverse function

--- ch4/slicing-exc/rev.go
+++ ch4/slicing-exc/rev.go
@@ -2,8 +2,19 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func reverse(s *[6]int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
 
 func main() {
-
+	a := [...]int{0, 1, 2, 3, 4, 5}
+	ra := &a
+	reverse(ra) // Reverses the order of the array
+	fmt.Println(ra)
 }
